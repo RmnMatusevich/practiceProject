7ec2289585bdeeac9b8ea7ee7d6033542c7bacb3
RmnMatusevich <47831494+RmnMatusevich@users.noreply.github.com>
2020-03-10 11:09:32 +0300

    Refactor search block
    ref: change widrth of search block

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -2,28 +2,16 @@
 const menu = document.getElementsByClassName('menu-button')[0];
 const navigation = document.getElementsByClassName('mob-navigation')[0];
 
-function openHide () {
+function openHide() {
     navigation.classList.toggle("navigation__active");
-    if(navigation.style.display === 'flex') {
+    if (navigation.style.display === 'flex') {
         navigation.style.display = 'none'
         navigation.style.top = "-100%";
-    }else {
+    } else {
         navigation.style.display = 'flex';
         navigation.style.top = "0";
     }
-    
+
 }
 
 menu.addEventListener('click', openHide);
-
-
-// const navigationItem = document.getElementsByClassName('navigation-item');
-
-
-
-// navigationItem[0].addEventListener('click', openHide);
-
-
-// for (let i = 0; i<navigationItem.length; i++) {
-//     navigationItem[i].addEventListener('click', openHide)
-// }
--- scripts/collage.js
+++ scripts/collage.js
@@ -29,65 +29,72 @@ const collageObj = [
         src: "https://spring.io/images/projects/spring-data-flow-9eb1733b76b6cd62cbdd9bc9a2b04e56.svg",
         tittle: "Spring Cloud Data Flow",
         description: "An orchestration service for composable data microservice applications on modern runtimes."
-    },
-    
+    }
+
 ];
 const collage = document.getElementsByClassName('collage')[0];
 
+window.onload = search;
 
-window.onload = function () {
-collageObj.map((i) => {
-    const collageItem = document.createElement('div');
-collageItem.className = 'collage-item';
-
-const img = document.createElement('img');
-img.className = 'collage-image';
-collageItem.append(img);
-
-const tittle = document.createElement('h6');
-tittle.className = 'collage-tittle';
-collageItem.append(tittle);
-
-const description = document.createElement('p');
-description.className = 'collage-description';
-collageItem.append(description);
-    img.src = i.src;
-    tittle.innerHTML = i.tittle;
-    description.innerHTML = i.description;
-    collage.append(collageItem);
-});
-}
 
 //SEARCH
 
+function loadItems(obj) {
+    obj.map((i) => {
+        const collageItem = document.createElement('div');
+        collageItem.className = 'collage-item';
+
+        const img = document.createElement('img');
+        img.className = 'collage-image';
+        collageItem.append(img);
+
+        const tittle = document.createElement('h6');
+        tittle.className = 'collage-tittle';
+        collageItem.append(tittle);
+
+        const description = document.createElement('p');
+        description.className = 'collage-description';
+        collageItem.append(description);
+        img.src = i.src;
+        tittle.innerHTML = i.tittle;
+        description.innerHTML = i.description;
+        collage.append(collageItem);
+    })
+};
 
 const input = document.getElementById('search-input');
 
-input.addEventListener('change',search);
- 
+input.addEventListener('input', search);
 
 
-function search() {
-    let regExp = new RegExp(`/${input.value}/`);
-    console.log(input.value);
-        
-    collageObj.map((i) => {
-        console.log(input.value);
-        console.log(i.description);
-        
-        if (i.description.indexOf(`${input.value}`) >= 0) {
-            console.log(`yes ${i.description.indexOf(input.value)}`);
+function search(i) {
+    let newCollageObj = [];
+    const collageItems = document.getElementsByClassName('collage-item');
 
-        }else {
-            collageObj.splice(collageObj.indexOf(i), 1)
-            
+    collageObj.map(i => {
+
+        if (i.description.includes(input.value)) {
+
+            newCollageObj.push(i);
+            for (let i = 0; i < collageItems.length; i++) {
+                collageItems[i].remove();
+            }
+
+            loadItems(newCollageObj);
+
+        } else {
+            for (let i = 0; i < collageItems.length; i++) {
+                collageItems[i].remove();
+            }
+            loadItems(newCollageObj);
+
         }
-        // if (i.description.match(regExp) == null){
-        //     console.log("no");
-            
-        // }else {
-        //     console.log("yes");
-            
-        // }
     })
+    const text = document.createElement('h1');
+    text.innerHTML = "No results!"
+    text.className = 'text';
+    // if (newCollageObj.length === 0) {
+    //     collage.append(text)
+    // }
 }
+
